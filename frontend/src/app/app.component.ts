3e89870c095500f0834e8dbc0581e847598045bf
Roman Zimnik <roman@software-crafter.com>
2021-01-30 18:20:59 +0100

    generator algorithm implemented

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,9 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  password: string = 'init';
+  length: number = 0;
+  password: string = '';
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
 
   onButtonClick() { 
-    this.password = 'myPW';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$&*()^';
+
+    let validChars = '';
+
+    if (this.includeLetters) { 
+      validChars += letters;
+    }
+    if (this.includeNumbers) { 
+      validChars += numbers;
+    }
+    if (this.includeSymbols) { 
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) { 
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  onChangeUseLetters() { 
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() { 
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() { 
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onChangeLength(event: Event) { 
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+    
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
   }
 }
