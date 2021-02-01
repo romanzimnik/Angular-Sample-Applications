7f9d502c1acf9c414cbdd321318775f72b8938a9
Roman Zimnik <roman@software-crafter.com>
2021-02-01 23:32:34 +0100

    cards application extended by properties binding

--- cards-application/src/app/app.component.ts
+++ cards-application/src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards-application';
+
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/images/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree.'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/images/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain!'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/images/biking.jpeg',
+      username: 'biker1990',
+      content: 'I did some biking today.'
+    }
+  ]
 }
