8b901636014d5fd1acd5e174679ebf264ecfa6e5
Roman Zimnik <roman@software-crafter.com>
2021-02-04 02:11:02 +0100

    paginator added

--- pages-and-directives-application/src/app/app.component.ts
+++ pages-and-directives-application/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages-and-directives-application';
+  currentPage: number = 0;
+  images = [
+    {
+      title: 'At the beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+    },
+    {
+      title: 'Holidays are great!',
+      url: 'https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'
+    },
+    {
+      title: 'Look at that water!',
+      url: 'https://images.unsplash.com/photo-1476673160081-cf065607f449?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80'
+    },
+    {
+      title: 'Great one, don\'t you think?',
+      url: 'https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+    }
+  ]
 }
