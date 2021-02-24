28f401780ae6f94cd72b5ba5ea346db0e63f081b
Roman Zimnik <roman@software-crafter.com>
2021-02-24 23:59:49 +0100

    reconfiguration to lazy loading modules

--- modules-and-routing-basics-application/src/app/app-routing.module.ts
+++ modules-and-routing-basics-application/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   { path: 'elements', loadChildren: () => import('./elements/elements.module').then(
     (m) => m.ElementsModule 
-  )}
+  )},
   { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent }
 ];
