f3407b07c1ffae6db9f98dc8a7420a455511fe85
Roman Zimnik <roman@software-crafter.com>
2021-02-25 02:36:09 +0100

    lazy loading for angular models integraded

--- modules-and-routing-basics-application/src/app/app-routing.module.ts
+++ modules-and-routing-basics-application/src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ const routes: Routes = [
   { path: 'elements', loadChildren: () => import('./elements/elements.module').then(
     (m) => m.ElementsModule 
   )},
+  { path: 'collections', loadChildren: () => import('./collections/collections.module').then(
+    (m) => m.CollectionsModule
+  )},
   { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent }
 ];
